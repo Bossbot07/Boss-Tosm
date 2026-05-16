e5742b14455e80b1585f9512797151e213107dc3
Bossbot07 <bogameing1@gmail.com>
2026-05-16 13:27:59 +0700

    Add files via upload

--- app.py
+++ app.py
@@ -192,7 +192,7 @@ HTML_TEMPLATE = """
             const timeInput = document.getElementById('modal-time-input').value;
             window.location.href = `/kill/${bossId}/${ch}?time_input=${timeInput}`;
         }
-        setInterval(() => { window.location.reload(); }, 10000);
+        setInterval(() => { window.location.reload(); }, 30000);
     </script>
 </body>
 </html>
